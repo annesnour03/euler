903eb95f333e9b1b077457b8fb1b07ec6c34da30
annesnour03 <annes.negmel-din@student.uva.nl>
2021-12-28 14:20:30 +0100

    28_number

--- 28_number_spiral_diagonals.py
+++ 28_number_spiral_diagonals.py
@@ -2,15 +2,25 @@ import time
 import sys
 import math
 def solve(size):
-    sum =  1
-    num = 1
-    for i in range(1,2*size):
-        sum = num + 2* i
-        print(sum)
+    current =  1
+    maal = 2
+    c = 1
+    res = [1]
+    # 1,3,5,7,13,17,21,25
+    for i in range(2*size):
+        if c % 5 == 0:
+            maal += 2
+        current = current + maal
+        res.append(current)
+        c += 1
+        print(res)
+    print(sum(res))
+
 def main():
     t0 = time.time()
     test = [2,4,6,8,12,16,20,24,30,36,42,48]
-    print(solve(5))
+    print(solve(7))
+    print([1,3,5,7,9,13,17,21,25,31,37,43,49])
     t1 = time.time()
 
     print("time = ","\x1b[6;30;42m",t1 - t0 , "\x1b[0m" )
