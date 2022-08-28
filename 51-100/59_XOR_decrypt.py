ab2a616309652b4c0043f1ecc84200e4ee346b0f
annesnour03 <annes.negmel-din@student.uva.nl>
2022-08-28 15:53:06 +0200

    59 xor decryption

--- 51-100/59_XOR_decrypt.py
+++ 51-100/59_XOR_decrypt.py
@@ -1,28 +1,52 @@
 import time
-import sys
-import math
 
-# need 9 bits
+
+def possible_keys():
+    possible_keys = []
+    start, end = ord('a'), ord('z')
+    for i in range(start, end):
+        for j in range(start, end):
+            for k in range(start, end):
+                possible_keys.append([i, j, k])
+    return possible_keys
+
+
+def xor_lists(x, y):
+    return [a ^ b for a, b in zip(x, y)]
+
+
+def generate_candidates(input):
+    keys = possible_keys()
+    for key in keys:
+        res = []
+        begin, end = 0, 3
+        while end <= len(input):
+            encrypted = input[begin:end]
+            res.extend(xor_lists(encrypted, key))
+            begin += 3
+            end += 3
+        to_ascii = "".join([chr(i) for i in res])
+        yield to_ascii
+
+
+def percentage_vowels(string):
+    return sum(map(string.lower().count, "aeiou"))/len(string)
+
+
 def solve(input):
-        for i in range(97,123):
-            for j in range(97,123):
-                for k in range(97,123):
-                    for num in input:
-                        total = i + j + k
-                        encry = chr(total ^ ord(num))
-                        if(encry ^ total == num):
-                            # print(chr(encry),end="")
-                            print(num)
-                print("")
-
+    candidates = generate_candidates(input)
+    vowels = []
+    # We will check if the % vowels is very low, that gives off garbage text.
+    for candidate in candidates:
+        vowels.append((percentage_vowels(candidate), candidate))
+    text = sorted(vowels, reverse=True)[0]
+    return sum([ord(i) for i in text[1]])
 
 
 def main():
     t0 = time.time()
-    file = list(open('59_input.txt','r').readline().split(','))
-    file = [chr(int(i)) for i in file]
-    print(file) 
-    ans = solve(file)
+    file = open('59_input.txt', 'r').readline().split(',')
+    ans = solve(list(map(int, file)))
     print(ans)
     print("time = ", "\x1b[2;30;42m", time.time() - t0, "\x1b[0m")
 
